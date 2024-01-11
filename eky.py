b3e99d4974b0fa4b74d703808f197b5d258ad097
He <xhe@xhe-37925s.nrel.gov>
2024-01-11 15:37:31 -0700

    str

--- eky.py
+++ eky.py
@@ -11,3 +11,7 @@ d = 5 + 5
 more_wrong_lines = (
     5 + np.cos(100.0) + np.sin(100.0) + np.sqrt(np.array([100.0, 200.0, 300.00]))
 )
+
+aa = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh" + str(
+    8743764734834834733437438478
+)
